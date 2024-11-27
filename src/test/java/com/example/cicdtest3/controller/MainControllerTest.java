a8438bb6c8ecd4e57939d503a4c06231ef0479af
hanbyul <unibyulse@gmail.com>
2024-11-27 15:32:45 +0900

    remove test code

--- src/test/java/com/example/cicdtest3/controller/MainControllerTest.java
+++ src/test/java/com/example/cicdtest3/controller/MainControllerTest.java
@@ -10,15 +10,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+//@SpringBootTest
+//@AutoConfigureMockMvc
 class MainControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
-
     @Test
     public void main() throws Exception {
-      mockMvc.perform(get("/"))
-          .andExpect(status().isOk());
     }
 }
