bbc52b863f07b3ae4753566e1a1099f13b71f0b3
hanbyul <unibyulse@gmail.com>
2024-11-27 12:21:56 +0900

    first commit

---
+++ src/test/java/com/example/cicdtest3/Cicdtest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdtest3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicdtest3ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
